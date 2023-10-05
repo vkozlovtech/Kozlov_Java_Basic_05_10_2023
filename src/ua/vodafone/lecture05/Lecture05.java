b0d5bcaf6183b1d5fe822dcb35c84877f51ede0d
Vitalii Kozlov <vkozlov.tech@gmail.com>
2023-10-05 13:45:42 +0300

    Initial commit

---
+++ src/ua/vodafone/lecture05/Lecture05.java
@@ -0,0 +1,19 @@
+package ua.vodafone.lecture05;
+
+public class Lecture05 {
+    public static void main(String[] args) {
+        int angles = 3;
+        ifElseDemo(angles);
+    }
+
+    private static void ifElseDemo(int angles) {
+
+        if (angles == 3) {
+            System.out.println("Triangle");
+        } else if (angles == 4) {
+            System.out.println("Square");
+        } else {
+            System.out.println("Unknown");
+        }
+    }
+}
