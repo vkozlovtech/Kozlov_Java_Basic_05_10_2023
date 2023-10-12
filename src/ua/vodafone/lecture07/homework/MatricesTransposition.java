70220a0e2009ba4f226cd3273a8743c739b65444
Vitalii Kozlov <vkozlov.tech@gmail.com>
2023-10-12 08:50:48 +0300

    L7HW11 add MatricesTransposition

---
+++ src/ua/vodafone/lecture07/homework/MatricesTransposition.java
@@ -0,0 +1,54 @@
+package ua.vodafone.lecture07.homework;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MatricesTransposition {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter matrix size:\n" + "Rows (M) = ");
+        int row = SCANNER.nextInt();
+        System.out.print("Columns (N) = ");
+        int column = SCANNER.nextInt();
+
+        int[][] matrix = generateMatrixElements(row, column);
+
+        System.out.println("\nOriginal matrix (M x N):\n");
+        displayMatrix(matrix);
+
+        System.out.println("\nTransposed matrix (N x M):\n");
+        displayMatrix(transposeOriginalMatrix(matrix, column, row));
+    }
+
+    private static int[][] transposeOriginalMatrix(int[][] matrix, int column, int row) {
+        int[][] transposedMatrix = new int[column][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
+    private static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int column : row) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] generateMatrixElements(int row, int column) {
+        int[][] matrix = new int[row][column];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ThreadLocalRandom.current().nextInt(100);
+            }
+        }
+        return matrix;
+    }
+}
