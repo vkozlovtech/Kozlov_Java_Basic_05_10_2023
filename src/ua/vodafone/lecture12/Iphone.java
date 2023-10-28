66df1da08c2db7e07a405e46b22ad67247a8e052
Vitalii Kozlov <vkozlov.tech@gmail.com>
2023-10-28 17:42:10 +0300

    L12HW19 add Interfaces

---
+++ src/ua/vodafone/lecture12/Iphone.java
@@ -0,0 +1,42 @@
+package ua.vodafone.lecture12;
+
+public class Iphone implements Smartphones, IOs {
+
+    private final String vendor;
+    private final String model;
+    private final double osVersion;
+
+    public Iphone(String vendor, String model, double osVersion) {
+        this.vendor = vendor;
+        this.model = model;
+        this.osVersion = osVersion;
+    }
+
+    @Override
+    public void call(String number) {
+        System.out.println(vendor + " " + model + " called the " + number + ".");
+    }
+
+    @Override
+    public void sms(String text) {
+        System.out.println(vendor + " " + model + " sent sms '" + text + "'.");
+    }
+
+    @Override
+    public void internet(String website) {
+        System.out.println(vendor + " " + model + " visited " + website + ".");
+    }
+
+    @Override
+    public void checkUpdateIOs() {
+        if (osVersion < 13) {
+            System.out.println("iOS version on " + vendor + " " + model +
+                    " is too old to update.");
+        } else if (osVersion < 17) {
+            System.out.println("iOS version on " + vendor + " " + model +
+                    " is lower than 17. You need to update.");
+        }   else {
+            System.out.println("Your " + vendor + " " + model + " has the latest iOS version.");
+        }
+    }
+}
---
+++ src/ua/vodafone/lecture12/Smartphones.java
@@ -0,0 +1,7 @@
+package ua.vodafone.lecture12;
+
+public interface Smartphones {
+    public void call(String number);
+    public void sms(String text);
+    public void internet(String website);
+}
