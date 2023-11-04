e257e1d025df7b06a4be44230d50dc83a258c81f
Vitalii Kozlov <vkozlov.tech@gmail.com>
2023-11-04 21:06:11 +0200

    L13HW18 PointTest comments correction

--- src/test/java/ua/vodafone/lecture10/PointTest.java
+++ src/test/java/ua/vodafone/lecture10/PointTest.java
@@ -6,64 +6,93 @@ import org.junit.jupiter.api.Test;
 public class PointTest {
 
     @Test
-    public void toOtherDistanceCalculation() {
+    public void toOtherEqualPoints() {
+        // given
         Point firstPoint = new Point(1, 2);
-        Point secondPoint = new Point(3, 4);
+        Point secondPoint = new Point(1, 2);
+
+        // when
         double result = firstPoint.calculateDistanceToOther(secondPoint);
-        Assertions.assertEquals(2.8284271247461903, result);
+
+        // then
+        Assertions.assertEquals(0, result, 0.001);
     }
 
     @Test
-    public void toOtherFirstPointNull() {
-        Point firstPoint = null;
+    public void toOtherDistanceCalculation() {
+        // given
+        Point firstPoint = new Point(1, 2);
         Point secondPoint = new Point(3, 4);
-        Assertions.assertThrows(NullPointerException.class, () -> firstPoint.calculateDistanceToOther(secondPoint));
+
+        // when
+        double result = firstPoint.calculateDistanceToOther(secondPoint);
+
+        // then
+        Assertions.assertEquals(2.8284271247461903, result, 0.001);
     }
 
     @Test
     public void toOtherSecondPointNull() {
+        // given
         Point firstPoint = new Point(1, 2);
         Point secondPoint = null;
-        double result = firstPoint.calculateDistanceToOther(secondPoint);
-        Assertions.assertEquals(-1, result);
-    }
 
-    @Test
-    public void toOtherBothPointsNull() {
-        Point firstPoint = null;
-        Point secondPoint = null;
-        Assertions.assertThrows(NullPointerException.class, () -> firstPoint.calculateDistanceToOther(secondPoint));
+        // when
+        double result = firstPoint.calculateDistanceToOther(secondPoint);
+
+        // then
+        Assertions.assertEquals(-1, result, 0.001);
     }
 
     @Test
     public void betweenTwoDistanceCalculation() {
+        // given
         Point firstPoint = new Point(3, 4);
         Point secondPoint = new Point(8, 8);
+
+        // when
         double result = Point.calculateDistanceBetweenTwo(firstPoint, secondPoint);
-        Assertions.assertEquals(6.4031242374328485, result);
+
+        // then
+        Assertions.assertEquals(6.4031242374328485, result, 0.001);
     }
 
     @Test
     public void betweenTwoFirstPointNull() {
+        // given
         Point firstPoint = null;
         Point secondPoint = new Point(8, 8);
+
+        // when
         double result = Point.calculateDistanceBetweenTwo(firstPoint, secondPoint);
-        Assertions.assertEquals(-1, result);
+
+        // then
+        Assertions.assertEquals(-1, result, 0.001);
     }
 
     @Test
     public void betweenTwoSecondPointNull() {
+        // given
         Point firstPoint = new Point(3, 4);
         Point secondPoint = null;
+
+        // when
         double result = Point.calculateDistanceBetweenTwo(firstPoint, secondPoint);
-        Assertions.assertEquals(-1, result);
+
+        // then
+        Assertions.assertEquals(-1, result, 0.001);
     }
 
     @Test
     public void betweenTwoBothPointsNull() {
+        // given
         Point firstPoint = null;
         Point secondPoint = null;
+
+        // when
         double result = Point.calculateDistanceBetweenTwo(firstPoint, secondPoint);
-        Assertions.assertEquals(-1, result);
+
+        // then
+        Assertions.assertEquals(-1, result, 0.001);
     }
 }
